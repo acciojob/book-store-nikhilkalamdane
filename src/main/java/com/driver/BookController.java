57e2dafe29e6752971d91a02cd924153496271eb
unknown <nikhilkalamdane99@gmail.com>
2023-01-05 18:25:49 +0530

    Completed 5 API

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,7 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -20,4 +17,39 @@ public class BookController {
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
     }
+
+    @GetMapping("/get-book-by-id/{id}")
+    public ResponseEntity findBookById(@PathVariable("id") String id){
+        return new ResponseEntity<>(bookService.findBookById(id), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/get-all-books")
+    public ResponseEntity findAllBooks(){
+        return new ResponseEntity<>(bookService.findAllBooks(), HttpStatus.FOUND);
+    }
+
+    @DeleteMapping("/delete-book-by-id/{id}")
+    public ResponseEntity deleteBookById(@PathVariable("id") String id){
+        bookService.deleteBookById(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/delete-all-books")
+    public ResponseEntity deleteAllBooks(){
+        bookService.deleteAllBooks();
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/get-books-by-author?author=author+name")
+    public ResponseEntity findBooksByAuthor(@RequestParam("author") String author){
+        bookService.findBooksByAuthor(author);
+        return new ResponseEntity<>(HttpStatus.FOUND);
+    }
+
+    @GetMapping("/get-books-by-genre?genre=genre+name")
+    public ResponseEntity findBooksByGenre(@RequestParam("genre") String genre){
+        bookService.findBooksByGenre(genre);
+        return new ResponseEntity<>(HttpStatus.FOUND);
+    }
+
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -6,28 +6,36 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BookRepository {
 
+    HashMap<Integer, Book> bookDB = new HashMap<>();
+
     public BookRepository(){
         
     }
 
     public Book save(Book book){
-        return null;
+        bookDB.put(book.getId(), book);
+        return book;
     }
 
     public Book findBookById(int id){
-
-        return null;
+        return bookDB.get(id);
     }
 
     public List<Book> findAll(){
-        return null;
+        List<Book> list = new ArrayList<>();
+        for(Book b: bookDB.values()){
+            list.add(b);
+        }
+        return list;
     }
 
     public void deleteBookById(int id){
+        bookDB.remove(id);
         return;
     }
 
     public void deleteAll(){
+        bookDB.clear();
         return;
     }
 
